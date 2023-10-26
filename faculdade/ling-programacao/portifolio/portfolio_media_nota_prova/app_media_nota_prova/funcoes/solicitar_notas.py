820f895f21c11c213cbe9fb5944dad4f5cb68cff
Erik Gonzaga <92276557+oErikGonzaga@users.noreply.github.com>
2023-10-25 21:40:25 -0300

    update portfolio

--- faculdade/ling-programacao/portifolio/portfolio_media_nota_prova/app_media_nota_prova/funcoes/solicitar_notas.py
+++ faculdade/ling-programacao/portifolio/portfolio_media_nota_prova/app_media_nota_prova/funcoes/solicitar_notas.py
@@ -1,12 +1,17 @@
-def solicitar_nota(mensagem):
+def solicitar_nota(nota_prova):
     while True:
         try:
-            entrada = input(mensagem)
+            entrada = input(nota_prova)
             nota = float(entrada.replace(',', '.'))
 
-            if 0 <= nota <= 10:
+            if not 0 <= nota <= 10:
                 return nota
             else:
                 print("Nota inválida. A nota deve estar entre 0 e 10.")
+
+            # Utilzando operador de negaçào para obter o mesmo resultado:
+            # if not (nota_prova1 > -1 and nota_prova1 < 11):
+            # print("Nota inválida. A nota deve estar entre 0 e 10.")
+
         except ValueError:
             print("Insira somente números.\nTente novamente.")
--- faculdade/ling-programacao/portifolio/portfolio_media_nota_prova/app_media_nota_prova/media_nota_prova.py
+++ faculdade/ling-programacao/portifolio/portfolio_media_nota_prova/app_media_nota_prova/media_nota_prova.py
@@ -1,11 +1,11 @@
-from app_media_nota_prova.funcoes.solicitar_notas import solicitar_nota
+from funcoes.solicitar_notas import solicitar_nota
 
 # Declarar as variáveis
-nota_prova1 = solicitar_nota("Digite a nota da Prova 1: ")
-nota_prova2 = solicitar_nota("Digite a nota da Prova 2: ")
+np1 = solicitar_nota("Digite a nota da Prova 1: ")
+np2 = solicitar_nota("Digite a nota da Prova 2: ")
 
 # Calcular a média
-media = (nota_prova1 + nota_prova2) / 2
+media = (np1 + np2) / 2
 
 # Exibir a média final
 print("Média Final:", media)
